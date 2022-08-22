35787838f835582c803b9b22cc7b6a26d27d9000
Carolina Nonato <csnonato@gmail.com>
2022-08-21 21:25:45 -0400

    Cart items component

---
+++ src/components/ShoppingCart.tsx
@@ -0,0 +1,25 @@
+import { Offcanvas, OffcanvasHeader, OffcanvasTitle, Stack } from "react-bootstrap"
+import { useShoppingCart } from "../context/ShoppingCartContext"
+import { CartItem } from "./CartItem"
+type ShoppingCartProps = {
+    isOpen: boolean
+}
+
+export function ShoppingCart({isOpen}: ShoppingCartProps){
+    const{closeCart, cartItems} = useShoppingCart()
+
+    return (
+    <Offcanvas show={isOpen} onHide={closeCart} placement="end">
+        <OffcanvasHeader closeButton>
+            <OffcanvasTitle>Cart</OffcanvasTitle>
+        </OffcanvasHeader>
+        <Offcanvas.Body>
+            <Stack gap={3}>
+                {cartItems.map(item => (
+                    <CartItem key={item.id} {...item} />
+                    ))}
+            </Stack>
+        </Offcanvas.Body>
+    </Offcanvas>
+    )
+}
--- src/context/ShoppingCartContext.tsx
+++ src/context/ShoppingCartContext.tsx
@@ -1,4 +1,5 @@
 import { createContext, ReactNode, useContext, useState } from "react";
+import { ShoppingCart } from "../components/ShoppingCart";
 
 type ShoppingCartProviderProps = {
     children: ReactNode
@@ -77,5 +78,6 @@ export function ShoppingCartProvider({children}: ShoppingCartProviderProps) {
 
     return <ShoppingCartContext.Provider value={{openCart, closeCart, cartItems, cartQuantity, getItemQuantity, increaseCartQuantity, decreaseCartQuantity, removeFromCart}}>
         {children}
+        <ShoppingCart isOpen={isOpen}/>
         </ShoppingCartContext.Provider>
 }
